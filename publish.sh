f71eedc073350ece48a228494eb7a57e84d1179d
fakeYanss <yanshisangc@gmail.com>
2018-02-04 21:51:15 +0800

    update gitbook

--- publish.sh
+++ publish.sh
@@ -5,12 +5,7 @@ git add . &&\
 git commit -m 'update gitbook' &&\
 git push origin master &&\
 git checkout gh-pages &&\
-git rm --cached -r . &&\
-git clean -df &&\
-git rm --cached -r . &&\
-git clean -df &&\
-rm -rf *~ &&\
-git checkout master -- publish.sh &&\
+rm -rf * &&\
 git checkout master -- _book &&\
 mv _book/* ./ &&\
 rm -rf _book &&\
